3dddf3475b55190e28115edff7797acdfa6ddb87
Rene TG <renetguerra@gmail.com>
2021-11-21 16:32:50 +0100

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }    
+        // public string Gender { get; set; }    
+        // public DateTime DateOfBirth { get; set; } 
+
+    }
+}
